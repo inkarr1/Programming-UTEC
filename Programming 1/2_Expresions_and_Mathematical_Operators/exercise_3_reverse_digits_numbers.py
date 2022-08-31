c395d1197b8ae3973386dd4d0ed8df3153a6ca6c
Jhanpiere Montes <jhanpieremontes@gmail.com>
2022-08-31 18:20:22 -0500

    Se organizó mejor las carpetas y archivos respecto a los temas en Python.

--- Programming 1/2_Expresions_and_Mathematical_Operators/exercise_3_reverse_digits_numbers.py
+++ Programming 1/2_Expresions_and_Mathematical_Operators/exercise_3_reverse_digits_numbers.py
@@ -1,7 +1,7 @@
 # Realice un programa que permita leer un número entero de 3 dígitos
 # y el programa imprima el número al revés.
 # Desarrolle el programa utilizando los operadores matemáticos y trabaje
-# bajo el supuesto que el usuario siempre ingresará un número de 3 dígitos
+# bajo el supuesto que el usuario siempre ingresará un número de 3 dígitos:
 
 num = int(input("Numero: "))
 
