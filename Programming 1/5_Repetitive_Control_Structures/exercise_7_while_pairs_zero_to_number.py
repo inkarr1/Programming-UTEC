cbf6ef70c0d9b232f6e2d66dce1f6d00de7df58d
Jhanpiere Montes <jhanpieremontes@gmail.com>
2022-09-01 12:52:03 -0500

    Se agregaron los ejercicios de laboratorio

---
+++ Programming 1/5_Repetitive_Control_Structures/exercise_7_while_pairs_zero_to_number.py
@@ -0,0 +1,10 @@
+# IMPRIME NÚMEROS PARES
+# Diseñe e implemente un algoritmo en Python que permita al usuario ingresar un número,
+# y el algoritmo debe imprimir los números pares, desde cero hasta el número ingresado.
+
+number = int(input("Ingrese el número: "))
+i = 0
+
+while i <= number:
+    print(i)
+    i = i + 2
