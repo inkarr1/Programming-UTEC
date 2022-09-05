c89444f8cbdeb2321c3939824cbd8c0024abc5a0
Jhanpiere Montes <jhanpieremontes@gmail.com>
2022-09-05 00:04:11 -0500

    Se agrego el quiz de CSI

---
+++ ICS/0_Homeworks_or_Evaluations/1_Quiz.py
@@ -0,0 +1,45 @@
+# 1.Cuantas veces aparecera 'Hola' en pantalla antes de que el proceso pase a estado terminate?
+
+# import os
+# i = 0
+# while i<10:
+#    print("Hola")
+
+# 2.En que estado se encontrara el proceso que ejecuta el siguiente programa cuando este en la instruccion senala con #A?
+import os
+pid = os.getpid()
+print(pid)
+key = input('press any key to continue')
+i = 0
+while i < 10 ** 6:
+    print(i)
+    i += 1
+    #A
+
+# 3.Cuantas veces apararecera 'Hola' en pantalla antes de que el proceso pase a estado terminated?
+# import os
+# i = 0
+# while i < 20:
+#     print('Hola')
+#     i = i + 1
+
+# 4.En que estado se encontrara el proceso que ejecuta el siguiente programa cuando llege a #A
+
+
+# 5. Indica la relacion con cada componente
+# 1.Application
+# 2.Operating System
+# 3.Hardware
+
+# 7. Indique la correspondencia con los estados de un proceso
+# a.New
+# b.Ready
+# c.Waiting/Sleeping
+# d.Running
+# e.Terminated
+
+# 9.El siguiente programa terminara cuando
+# import os
+# i = 0
+# while i < 20:
+#     print('Hola')
