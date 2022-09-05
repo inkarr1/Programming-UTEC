2b0088f27779595bf59eebce87b6a34329acb494
Jhanpiere Montes <jhanpieremontes@gmail.com>
2022-09-04 21:16:32 -0500

    Se agrego las preguntas del quiz de variables y expresiones

---
+++ Programming 1/0_Homeworks_or_Evaluations/1_Quiz.py
@@ -0,0 +1,48 @@
+# Variables y Expresiones
+
+# 1. De que tipo es la variable x?
+# x = 43
+# print(x)
+
+# 2. De que tipo es la variable y?
+# y = input('Ingrese el valor de y')
+# print(y)
+
+# 3.Cual es la instruccion?
+# edad = 18
+# print(edad)
+
+# 4.Cual sera el valor final de la variable c?
+# a = 3
+# b = 4
+# c = (a ** 2 + b ** 2) ** (1 / 2)
+# print(c)
+
+# 5.Cual es la expresion que define la siguiente formula matematica?
+# d = 12.25
+# r = 35.13
+# n = 2
+# e = d * (1 + (r/100)) ** n
+# print(e)
+
+# 6. En siguiente fragmento de codigo, Cual es le valor de la variable c?
+# f = 100%10
+# print(f)
+
+# 7.Cual es la expresion adecuada para la siguiente formula matematica:
+# z = (w + v)/2
+# print(z)
+
+# 8.Cual es la expresion adecuada para la siguiente formula matematica:
+# g = (h ** 2 + i ** 2) ** (1/2)
+# print(j)
+
+# 9.Si n es un numero positivo, cual es el valor de c en el siguiente fragmento de codigo:
+# k = int(input("Ingrese el valor de n: "))
+# l = (k // 10) % 10
+# print(l)
+
+# 10.Para que valores de n, c tendra el valor de 1
+p = int(input("Ingrese el valor de n: "))
+q = p % 2
+print(q)
