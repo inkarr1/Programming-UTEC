6fbae3aad60933901af000c32b25cb7940f58262
Jhanpiere Montes <jhanpieremontes@gmail.com>
2022-09-06 18:59:53 -0500

    Se agregó la Tarea 1.1 de Teoría

---
+++ Programming 1/0_Homeworks_or_Evaluations/Homework_Theory/solution.py
@@ -0,0 +1,59 @@
+
+# NO MODIFICAR NADA BAJO ESTA LINEA
+def main():
+# NO MODIFICAR NADA SOBRE ESTA LINEA
+
+    #============Pregunta 1============
+    N = input("Nombre: ")
+
+    # Codigo para Pregunta 1 comienza aqui
+
+    print("")
+
+    # Codigo para Pregunta 1 acaba aqui
+
+    #============Pregunta 2============
+
+    N = int(input("N: "))
+
+    # Codigo para Pregunta 2 comienza aqui
+
+    print("")
+
+
+    # Codigo para Pregunta 2 acaba aqui
+    #============Pregunta 3============ 
+    
+    N = int(input("N: "))
+    M = int(input("M: "))
+    # Codigo para Pregunta 3 comienza aqui
+
+    print("")
+
+    # Codigo para Pregunta 3 acaba aqui
+    
+
+    #============Pregunta 4============ 
+    
+    N = float(input("N: "))
+
+    # Codigo para Pregunta 4 comienza aqui
+
+    print("")
+
+    # Codigo para Pregunta 4 acaba aqui
+
+    #============Pregunta 5============ 
+    
+    N = str(input("Personaje: "))
+
+    # Codigo para Pregunta 5 comienza aqui
+    print("")
+
+    # Codigo para Pregunta 5 acaba aqui
+
+
+# NO MODIFICAR NADA BAJO ESTA LINEA
+if __name__ == '__main__':
+    main()
+# NO MODIFICAR NADA SOBRE ESTA LINEA
