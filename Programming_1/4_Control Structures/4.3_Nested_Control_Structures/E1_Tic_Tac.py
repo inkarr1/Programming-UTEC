5896513afd30dd6c8af91b771c0543d5a78c9ef7
inkarri <jhanpieremontes@gmail.com>
2023-04-20 17:40:44 -0500

    Mejoras al ejercicio E1-TicTac

--- Programming_1/4_Control Structures/4.3_Nested_Control_Structures/E1_Tic_Tac.py
+++ Programming_1/4_Control Structures/4.3_Nested_Control_Structures/E1_Tic_Tac.py
@@ -1,5 +1,5 @@
 num_input = 0
-while num_input<= 0 or number_input > 500:
+while num_input <= 0 or num_input > 500:
     num_input = int(input("Número [1 - 500]: "))
 
 i = 0
@@ -7,9 +7,10 @@ while i < num_input:
     i += 1
     if i % 4 == 0 and i % 6 == 0:
         print(i, "TicTac")
-    elif i % 6 == 0:
-        print(i, "Tac")
     elif i % 4 == 0:
         print(i, "Tic")
+    elif i % 6 == 0:
+        print(i, "Tac")
     else:
         print(i)
+print("Fin")
